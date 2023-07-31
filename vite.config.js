2d1b1b9ae5e981124f05b30f1a571cf5b6665a90
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2023-07-31 22:42:46 +0300

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,16 @@
-// import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   // plugins: [vue()],
-//   // resolve: {
-//   //   alias: {
-//   //     "@": fileURLToPath(new URL("./src", import.meta.url)),
-//   //   },
-//   // },
-//   resolve: {
-//     alias: [
-//         {
-//             find: '@',
-//             replacement: path.resolve(__dirname, 'src')
-//         }
-//     ]
-// },
-// });
-
 export default defineConfig({
-  server: {
-    plugins: [vue()],
-    base: "/new-cocktails",
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
+ 
 });
+
