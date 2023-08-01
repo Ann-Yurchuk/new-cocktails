6014b2c074b62afb3ab0762c9c834484509b5a36
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2023-08-01 22:02:38 +0300

    update index

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Cocktail from '../pages/Cocktail.vue'
+
 // import CocktailRandom from '../pages/CocktailRandom.vue'
 
 const router = createRouter({
